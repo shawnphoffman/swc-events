a66efe76781d933e1b0f731a251c2e44cf764016
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-03-13 16:34:51 -0700

    reeeee

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,42 @@ import { useState } from 'react'
 import EventProvider from 'context/EventContext'
 import FavoritesProvider from 'context/FavoritesContext'
 import Nav from 'components/Nav'
+import { styled } from 'linaria/react'
+
+// height: ${() => window.innerHeight}px;
+const AppWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100vh;
+	align-items: center;
+	padding: 0 8px 8px 8px;
+	background: var(--bg);
+
+	@media print {
+		background: none;
+		color: black;
+		padding: 0;
+		height: auto;
+		position: relative;
+		display: block;
+	}
+`
 
 function MyApp({ Component, pageProps }) {
 	const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
 	return (
-		<SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-			<EventProvider>
-				<FavoritesProvider>
-					<Nav />
-					<Component {...pageProps} />
-				</FavoritesProvider>
-			</EventProvider>
-		</SessionContextProvider>
+		<AppWrapper>
+			<SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+				<EventProvider>
+					<FavoritesProvider>
+						<Nav />
+						<Component {...pageProps} />
+					</FavoritesProvider>
+				</EventProvider>
+			</SessionContextProvider>
+		</AppWrapper>
 	)
 }
 
