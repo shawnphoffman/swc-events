c25671afd15a45995c37b946dfb0a8287d381707
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-03-15 17:02:20 -0700

    api cache

--- pages/api/vendors.js
+++ pages/api/vendors.js
@@ -3,33 +3,16 @@ import { processApiVendors } from 'utils/dataUtils'
 const url = process.env.NEXT_PUBLIC_VENDOR_ENDPOINT
 
 export default async function handler(req, res) {
-	// try {
 	const resp = await fetch(url)
 	const json = await resp.json()
 
-	// console.log(json)
-	console.log('FIRE')
-
 	const { vendors, tattooArtists } = processApiVendors(json)
+
+	res.setHeader('Cache-Control', 'public, s-maxage=300, stale-while-revalidate=599')
 
 	res.status(200).json({
 		vendors,
 		tattooArtists,
 	})
 	return
-	// } catch (e) {
-	// 	console.log(e)
-
-	// 	import('data/schedule.json').then(j => console.log(j))
-
-	// 	const { events, venues } = processApiData(json)
-	// 	res.status(200).json({
-	// 		events,
-	// 		venues,
-	// 	})
-	// 	// dispatch({ type: EventAction.SET_EVENTS, name: events, venues: venues, disabled: disabledVenues })
-	// 	return
-	// }
-
-	// res.status(200).json({ name: 'John Doe' })
 }
