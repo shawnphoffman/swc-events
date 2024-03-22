d20157f5bb90edc3dfe09e5761ad818f751eaccf
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-21 18:44:22 -0700

    center

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default async function LandingPage() {
 	return (
-		<div className="flex flex-col flex-1 w-full max-w-4xl px-3">
+		<div className="flex flex-col flex-1 w-full max-w-4xl px-3 justify-center items-center">
 			<h1>Exciting things...</h1>
 			<h3>are coming...</h3>
 		</div>
